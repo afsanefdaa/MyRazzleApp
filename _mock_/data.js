af7d4d7c296c2fae5834f4bc4544de8e53d0fc2e
a.fadaei <a.fadaei@kian.digital>
2019-12-28 16:32:31 +0330

    test dynamic helmet on razzle

---
+++ _mock_/data.js
@@ -0,0 +1,7 @@
+const ceo = {
+  title: 'fetch title',
+  description: 'fetch description',
+  image: 'https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwj8jq6gs9jmAhXM-6QKHRbhDkAQjRx6BAgBEAQ&url=https%3A%2F%2Fcreate-react-app.dev%2Fdocs%2Fadding-images-fonts-and-files%2F&psig=AOvVaw0_i-HBDC8vcqsj4LH1OnUh&ust=1577624465196590'
+};
+
+export default ceo;
--- src/app/pages/Home/Home.js
+++ src/app/pages/Home/Home.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import style from './Home.module.scss';
+import { Helmet } from 'react-helmet';
+import ceo from '../../../../_mock_/data';
+// import style from './Home.module.scss';
 
 class Home extends React.Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    title: '',
+    description: '',
+    image: '',
+  };
+
+  componentDidMount() {
+    this.setState({
+      title: ceo.title,
+      description: ceo.description,
+      image: ceo.image,
+    });
+  }
+
   render() {
     return (
-      <div className={style.Home}>
+      <div>
+        <Helmet>
+          <title>{this.state.title ? this.state.title : 'Volunteer Hub by Indorelawan'}</title>
+          <meta name="title" content={this.state.title ? this.state.title : 'Volunteer Hub by Indorelawan'} />
+          <meta
+            name="description"
+            content={this.state.description ? this.state.description : 'Volunteer Hub by Indorelawan adalah tempat kolaborasi antara relawan dan komunitas sosial yang memiliki semangat kerelawanan dan gotong royong untuk Indonesia.'}
+          />
+          <meta
+            property="og:title"
+            content={this.state.title ? this.state.title : 'Volunteer Hub by Indorelawan'}
+          />
+          <meta
+            property="og:description"
+            content={this.state.description ? this.state.description : 'Volunteer Hub by Indorelawan adalah tempat kolaborasi antara relawan dan komunitas sosial yang memiliki semangat kerelawanan dan gotong royong untuk Indonesia.'}
+          />
+          <meta
+            property="og:image"
+            content={this.state.image ? this.state.image : 'https://volunteerhub.id/assets/logo/seo.jpg'}
+          />
+          <meta property="og:url" content="https://volunteerhub.id" />
+          <meta
+            name="twitter:title"
+            content={this.state.title ? this.state.title : 'Volunteer Hub by Indorelawan'}
+          />
+          <meta
+            name="twitter:description"
+            content={this.state.description ? this.state.description : 'Volunteer Hub by Indorelawan adalah tempat kolaborasi antara relawan dan komunitas sosial yang memiliki semangat kerelawanan dan gotong royong untuk Indonesia.'}
+          />
+          <meta
+            name="twitter:image"
+            content={this.state.image ? this.state.image : 'https://volunteerhub.id/assets/logo/seo.jpg'}
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Helmet>
+
         Home
       </div>
     );
